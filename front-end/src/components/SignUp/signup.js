462192bad77bb98924a38b94f4cf5db5c96626ce
zeecnla <csrmelchor@gmail.com>
2019-05-05 18:38:07 -0700

    Removed mongodb and used lowdb instead; everything is done

--- front-end/src/components/SignUp/signup.js
+++ front-end/src/components/SignUp/signup.js
@@ -7,12 +7,59 @@ export default class SignUp extends React.Component {
 
     constructor (){
         super();
-        this.submit = this.submit.bind(this);
+        this.state = {
+            username:'',
+            firstname: '',
+            lastname: '',
+            email: '',
+            password:''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeUserName = this.handleChangeUserName.bind(this);
+        this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
+        this.handleChangeLastName = this.handleChangeLastName.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
         
     }
-
-    submit(event){
-        console.log("submitted");
+    handleChangeUserName(event) {
+        this.setState({username:event.target.value});
+    }
+    handleChangeFirstName(event) {
+        this.setState({firstname: event.target.value});
+    }
+    handleChangeLastName(event) {
+        this.setState({lastname: event.target.value});
+    }
+    handleChangeEmail(event) {
+        this.setState({email: event.target.value});
+    }
+    handleChangePassword(event) {
+        this.setState({password: event.target.value});
+    }
+    //call api here
+    handleSubmit(event){
+        console.log('submitting');
+        if( this.state.username     &&
+            this.state.firstname    &&
+            this.state.lastname     &&
+            this.state.email        &&
+            this.state.password){
+                console.log(this.state);
+                const data = this.state;
+                fetch(`http://localhost:2093/users/register`, {
+                    method: "POST", 
+                    mode: "no-cors",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    body: JSON.stringify(this.state)
+                })
+                .then(response=> console.log(response))
+                .catch(error => console.error('Error:', error));
+                
+            }
         event.preventDefault();
     }
 
@@ -21,11 +68,12 @@ export default class SignUp extends React.Component {
     return(
         <div className="signup">
             <h2>Sign Up!</h2>
-            <form onSubmit={this.submit}>
-                <input type="text" placeholder="First Name" required/>
-                <input type="text" placeholder="Last Name" required/>
-                <input type="email" placeholder="Email" required/>
-                <input type="password" placeholder="Password" required/>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.username} onChange={this.handleChangeUserName} placeholder= "User Name" required/>
+                <input type="text" value= {this.state.firstName} onChange={this.handleChangeFirstName} placeholder="First Name" required/>
+                <input type="text"  value= {this.state.lastName} onChange={this.handleChangeLastName}  placeholder="Last Name" required/>
+                <input type="email" value= {this.state.email}  onChange={this.handleChangeEmail} placeholder="Email" required/>
+                <input type="password" value= {this.state.password} onChange={this.handleChangePassword}   placeholder="Password" required/>
                 <input id="submit" type="submit"/>
             </form>
             <p>Already have an account? <Link to="/login">Login</Link></p>
